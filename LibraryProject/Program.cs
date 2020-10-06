3639acbdc2610ca31cc90b6bb4b0100665051dd3
Elena Vonder Meulen <evonder13@gmail.com>
2020-10-06 13:50:15 -0400

    Add project files.

---
+++ LibraryProject/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Web;
+using Education.Db;
+using UtilityLibrary;
+
+namespace LibraryProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var about = Utility.About();
+            Console.WriteLine($"About is {about}");
+
+            var stud1 = new Student()
+            {
+                Id = 1,
+                Name = "Fred",
+                SAT = 1000,
+                GPA = 2.5
+            };
+            
+        }
+    }
+}
---
+++ UtilityLibraryProject/Utility.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UtilityLibrary
+{
+    public class Utility
+    {
+        public static string About()
+        {
+            return "This is Utility Library";
+        }
+    }
+}
